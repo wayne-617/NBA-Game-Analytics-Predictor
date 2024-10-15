143f7b8dd5a9798ad05d6d6abc3e3b9f3a4708d2
wayne-617 <wgl91603@uga.edu>
2024-10-15 00:27:16 -0400

    added scrape_season() - fetch box score tables

--- get_data.py
+++ get_data.py
@@ -9,6 +9,7 @@ DATA_DIR = "data"
 STANDINGS_DIR = os.path.join(DATA_DIR, "standings")
 SCORES_DIR = os.path.join(DATA_DIR, "scores")
 
+# grab specified html give the url and selector
 async def get_html(url, selector, sleep = 5, retries = 3):
     html = None
     for i in range(1, retries + 1):
@@ -28,11 +29,36 @@ async def get_html(url, selector, sleep = 5, retries = 3):
             break
     return html
     
-season = 2016
-url = f"https://www.basketball-reference.com/leagues/NBA_{season}_games.html"
+# scrape links for the box score tables in that season
+async def scrape_season(season):
+    url = f"https://www.basketball-reference.com/leagues/NBA_{season}_games.html"
+    html = asyncio.run(fetch_data(url))
+
+    soup = BeautifulSoup(html)
+    links = soup.find_all("a") # get anchor tags
+    href = [l["href"] for l in links] # parse links
+    standings_pages = [f"https://basketball-reference.com{l}" for l in href] # transform links into full urls
+
+    for url in standings_pages:
+        path = os.path.join(STANDINGS_DIR, url.split("/")[-1]) # create path to file based off url
+        if os.path.exists(path):
+            continue # continue if page is already scraped
+
+        html = await get_html(url, "#all_schedule") # grab html for standings table
+        with open(path, "w+") as f:
+            f.write(html)
+
+
 
 async def fetch_data(url):
     html = await get_html(url, "#content .filter")
     print(html)
+    return html
 
-asyncio.run(fetch_data(url))
+async def fetch_box_scores(season):
+    await scrape_season(season)
+
+for season in SEASONS:
+    fetch_box_scores(season)
+
+
